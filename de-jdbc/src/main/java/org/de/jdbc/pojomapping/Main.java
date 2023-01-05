2dbf3199dd65a030c427ea29e97fe38f94764a04
JHwan96 <dlwnghks00@gmail.com>
2023-01-05 22:52:04 +0900

    study jdbc_1

---
+++ de-jdbc/src/main/java/org/de/jdbc/pojomapping/Main.java
@@ -0,0 +1,32 @@
+package org.de.jdbc.pojomapping;
+
+import org.de.jdbc.mapper.Product;
+import org.de.jdbc.mapper.ResultSetMapper;
+
+import java.util.*;
+import java.sql.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        try {
+//here de-jdbc is database name, root is username and password is null. Fix them for your database settings.
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/de-jdbc", "root", "1234");
+            DatabaseMetaData databaseMetaData = con.getMetaData();
+            System.out.println(databaseMetaData.getDriverName() + ", " + databaseMetaData.getDriverVersion());
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from product");
+
+            List<Product> products = new ArrayList<>();
+            while (rs.next()) {
+                Product product = ResultSetMapper.create(rs);
+                products.add(product);
+            }
+
+            products.stream().forEach(it -> it.setPrice(it.getPrice() - 1000));
+
+            con.close();
+        } catch (Exception e) {System.out.println(e);}
+    }
+}
